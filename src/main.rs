9ed5315cc8cf9673f5f4c02b69f6dd147617b586
Duncan Vriend <vriendduncan@gmail.com>
2024-05-14 09:26:18 +0200

    functional program

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,90 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+use std::ffi::OsString;
+use std::io::Result;
+
 fn main() {
-    println!("Hello, world!");
+    use std::env::args_os;
+
+    let input = args_os().skip(1); // first element is a garbage/undefined value;
+                                   // everything after is a command line given arg.
+
+    handle_paths(input);
+}
+
+fn handle_paths(paths: impl Iterator<Item = OsString>) {
+    for path in paths {
+        handle_file(&path).unwrap_or_else(|e| {
+            eprintln!("{path:?}: {e}");
+        });
+    }
+}
+
+fn handle_file(path: &OsString) -> Result<()> {
+    use std::fs::File;
+    use std::io::{Read, Seek, Write};
+
+    let mut text = String::new();
+
+    let mut file = File::options().read(true).write(true).open(path)?;
+
+    // TODO: deal with [io::ErrorKind::OutOfMemory]
+    file.read_to_string(&mut text)?;
+    correct_spaces(&mut text);
+    file.set_len(text.len() as u64)?;
+    file.rewind()?;
+    file.write_all(text.as_bytes())
+}
+
+fn correct_spaces(text: &mut String) {
+    let evil_sigils = ["!", "=", "<", ">"];
+    let mut i = 1usize;
+    'Outer: loop {
+        if i >= text.len() {
+            return;
+        }
+        if text.get(i..=i).unwrap_or_default() == "<"
+            && !evil_sigils.contains(&text.get(i + 1..=i + 1).unwrap_or_default())
+        // Do not enter on Wikipedia comments, or equations (e.g. <!-- comment -->, <=)
+        {
+            loop {
+                i += 1;
+                if i >= text.len() {
+                    return;
+                }
+                if text.get(i - 1..=i).unwrap_or_default() == "</"
+                // This also deals with self-closing blocks like '</ref name="">'
+                {
+                    loop {
+                        i += 1;
+                        if i >= text.len() {
+                            return;
+                        }
+                        if text.get(i - 1..i).unwrap_or_default() == ">" {
+                            continue 'Outer;
+                        }
+                    }
+                }
+            }
+        }
+        if text.get(i - 1..=i).unwrap_or_default() == "  " {
+            let space_start = i - 1;
+            let space_end;
+            loop {
+                i += 1;
+                if text.get(i..=i).unwrap_or_default() != " " {
+                    space_end = i - 1;
+                    break;
+                }
+            }
+            text.replace_range(space_start..=space_end, " ");
+            i = space_start;
+            #[cfg(debug_assertions)]
+            println!("removed spaces '{space_start}–{space_end}'");
+            continue;
+        }
+
+        i += 1;
+    }
 }
